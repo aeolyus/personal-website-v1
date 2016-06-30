5e54918cfbb5e9c7745ddf8ecf334b3052218664
Richard Huang <richardhuang.huang@gmail.com>
2016-06-30 13:58:29 -0700

    fixes #5

--- components/main.js
+++ components/main.js
@@ -6,16 +6,6 @@ $( document ).ready(function(){
   $('.parallax').parallax();
 });
 
-//
-$(function() {
-     var pgurl = window.location.href.substr(window.location.href
-.lastIndexOf("/")+1);
-     $("#nav ul li a").each(function(){
-          if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
-          $(this).addClass("active");
-     })
-});
-
 $(function(){
       $("#texttyped").typed({
         strings: ["a high school student.", "a hacker.", "an award-winning artist.", "a programmer.", "a painter", "a photographer.", "a web designer.", "a robotics programming lead.", "a kungfu black belt.", "an AR and VR enthusiast."],
