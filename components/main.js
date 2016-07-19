3e4f211156235e1da77f7e7926e1369874a6f1f9
Richard Huang <richardhuang.huang@gmail.com>
2016-07-18 23:21:33 -0700

    added icons

--- components/main.js
+++ components/main.js
@@ -1,15 +1,15 @@
-$( document ).ready(function(){
-  $(".button-collapse").sideNav();
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
 });
 
-$( document ).ready(function(){
-  $('.parallax').parallax();
+$(document).ready(function() {
+    $('.parallax').parallax();
 });
 
-$(function(){
-      $("#texttyped").typed({
-        strings: ["a high school student.", "a hacker.", "an award-winning artist.", "a programmer.", "a painter", "a photographer.", "a web designer.", "a robotics programming lead.", "a kungfu black belt.", "an AR and VR enthusiast."],
+$(function() {
+    $("#texttyped").typed({
+        strings: ["a high school student.", "a hacker.", "an award-winning artist.", "a programmer.", "an painter", "a photographer.", "a web designer.", "a robotics programming lead.", "a kungfu black belt.", "an AR and VR enthusiast.", "a cryptography enthusiast"],
         typeSpeed: 20,
         loop: true
-      });
-  });
+    });
+});
