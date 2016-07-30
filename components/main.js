2aacd76783c0f027b3066a1f26ddcffcb99b7212
Richard Huang <richardhuang.huang@gmail.com>
2016-07-29 21:05:40 -0700

    I can do things

--- components/main.js
+++ components/main.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
     $('.parallax').parallax();
 });
 
+$(document).ready(function() {
+    $('.collapsible').collapsible({
+        accordion: false
+    });
+});
+
 $(function() {
     $("#texttyped").typed({
         strings: ["a high school student.", "a hacker.", "an award-winning artist.", "a programmer.", "an painter", "a photographer.", "a web designer.", "a robotics programming lead.", "a kungfu black belt.", "an AR and VR enthusiast.", "a cryptography enthusiast", "a hooman."],
