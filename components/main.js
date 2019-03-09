58fa18cac08878d67b14068035c0124ad1122ee6
Richard Huang <richardhuang.huang@gmail.com>
2019-03-09 00:18:08 -0800

    some info updates

--- components/main.js
+++ components/main.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
 $(function() {
     $("#texttyped").typed({
-        strings: ["a high school student.", "a hacker.", "an award-winning artist.", "a programmer.", "a painter.", "a photographer.", "a web designer.", "a robotics programming lead.", "a kungfu/wushu black belt.", "an AR and VR enthusiast.", "a cryptography enthusiast.", "a hooman."],
+        strings: ["a student at UC Berkeley.", "a hacker.", "an award-winning artist.", "a programmer.", "a painter.", "a photographer.", "a web designer.", "a sysadmin", "a kungfu/wushu black belt.", "an AR and VR enthusiast.", "a cryptography enthusiast.", "a hooman."],
         typeSpeed: 20,
         loop: true
     });
